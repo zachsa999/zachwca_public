7adbd3ba62095030967d3fc04fbed871f78e454b
zachsa999 <zach@zachw.ca>
2022-04-20 20:17:07 -0500

    change things

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,14 @@
 <script>
-	$: mouse = { x: 0, y: 0 };
-
-	$: outerWidth = 0;
-	$: innerWidth = 0;
-	$: outerHeight = 0;
-	$: innerHeight = 0;
-	$: center = { x: innerHeight / 2, y: outerHeight / 2 };
-	$: distanceFromCenter = mouse.x + mouse.y / 2;
-
-	function handleMouseMove(event) {
-		mouse.x = (event.clientX - innerWidth / 2) / 100;
-		mouse.y = (event.clientY - innerHeight / 2) / 100;
-	}
 </script>
 
-<svelte:window bind:innerWidth bind:outerWidth bind:innerHeight bind:outerHeight />
-
-<section on:mousemove={handleMouseMove} class="font-ubuntu flex justify-center items-center ">
+<div class="font-ubuntu flex justify-center items-center w-full" id="layer">
 	<div
 		class="max-w-4xl content-wrapper flex justify-center items-center 
         h-screen m-5"
 	>
 		<div
-			class="card bg-white px-28 py-8 shadow-2xl rounded-3xl"
-			style={`transform: rotate3d(${mouse.x}, ${mouse.y}, 1, ${distanceFromCenter}deg);`}
+			class="card bg-white px-28 py-8 shadow-2xl rounded-3xl border-slate-200 border-solid border"
+			id="card"
 		>
 			<img src="zWebLogo.png" alt="zWeb Logo" style="" class="w-full mb-8" />
 			<div class="w-full px-5">
@@ -41,14 +26,10 @@
 					</div>
 				</div>
 			</div>
-			<p>x={mouse.x} y={mouse.y}</p>
 		</div>
 		<div />
 	</div>
-</section>
+</div>
 
 <style>
-	.card {
-		/* transform: skew({mouse.x}); */
-	}
 </style>
